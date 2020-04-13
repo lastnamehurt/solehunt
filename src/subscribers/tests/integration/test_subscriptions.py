3dd390084cb5ab13b06b961419c34cd726aeaddc
lastnamehurt <clhurt1@gmail.com>
2020-04-12 20:26:12 -0700

    adds Subscription Tests

---
+++ src/subscribers/tests/integration/test_subscriptions.py
@@ -0,0 +1,22 @@
+"""
+RULE: must follow SoleHunt IG & Twitter to be subscribed
+"""
+from unittest import TestCase
+
+
+class SubscriptionTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def testSubscribed(self):
+        pass
+
+    def testUnsubscribed(self):
+        pass
+
+    def testPendingSubscription(self):
+        pass
+
+    def testInvalidSubscription(self):
+        pass
