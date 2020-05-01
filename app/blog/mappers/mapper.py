2070d316919b23c6dc6aabdb17f3415eae75b522
lastnamehurt <clhurt1@gmail.com>
2020-04-30 23:40:22 -0700

    updates mapper

--- app/blog/mappers/mapper.py
+++ app/blog/mappers/mapper.py
@@ -7,8 +7,8 @@ class GhostApiMapper:
     # TODO: churt make this better than O(n) size with O(n+1) space. It's ugly
     @classmethod
     def toBlogModel(cls, apiResponses: ResponseType.GET_POSTS) -> ResponseType.BLOG_POSTS:
-        newBlogs = []
         postsData = apiResponses['posts']
+        newBlogs = [None] * len(postsData)
         for postData in postsData:
             data = cls.parseBlogPost(postData)
             newBlogs.append(blogService.prepare(filters=data))
